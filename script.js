7df69c23470ded5c4ee847266890bcb0705e3866
anuj2230 <anujaqua72@gmail.com>
2024-12-21 16:42:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,307 @@
+var arr = [{
+    name:'sarthak',
+    Followers: 50000,
+    Media:43,
+    Following:500,
+    imge1:'https://images.unsplash.com/photo-1729731322082-c0a7c32229aa?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    img2:'https://images.unsplash.com/photo-1724961766924-b4574a0ef4d7?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+ },
+ {
+    name:'anuj',
+    Followers: 70000,
+    Media:64,
+    Following:780,
+    imge1:'https://images.unsplash.com/photo-1726266852957-b1549c5c9fcb?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    img2:'https://images.unsplash.com/photo-1729717069574-e25f4bb69d87?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+ },
+ {
+    name:'prateek',
+    Followers: 10000,
+    Media:54,
+    Following:340,
+    imge1:'https://images.unsplash.com/photo-1731370963500-b836d108f7c9?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    img2:'https://images.unsplash.com/photo-1729762343419-8fd0be625fb6?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+ },{
+    name:'sahil',
+    Followers: 50000,
+    Media:43,
+    Following:989,
+    imge1:'https://images.unsplash.com/photo-1731426899986-6506cc1beb4f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    img2:'https://images.unsplash.com/photo-1733895422316-d4aafcb1be03?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+ },
+ {
+    name:'Prabha',
+    Followers: 70000,
+    Media:64,
+    Following:765,
+    imge1:'https://images.unsplash.com/photo-1731156639378-1b3170abb35d?q=80&w=1370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    img2:'https://images.unsplash.com/photo-1719937050792-a6a15d899281?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+ 
+ },
+ {
+    name:'Chai',
+    Followers: 10000,
+    Media:54,
+    Following:200,
+    imge1:'https://images.unsplash.com/photo-1731574573959-993dda686d36?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    img2:'https://images.unsplash.com/photo-1732365319544-4b09b3bbf6f4?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+ },{
+    name:'Biscuit',
+    Followers: 50000,
+    Media:43,
+    Following:450,
+    imge1:'https://images.unsplash.com/photo-1715775491748-ecf23be19815?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    img2:'https://images.unsplash.com/photo-1733855803980-eeed78f64836?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+ },
+ {
+    name:'bhavesh',
+    Followers: 70000,
+    Media:64,
+    Following:800,
+    imge1:'https://images.unsplash.com/photo-1734120284108-e5e0ecdb738b?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    img2:'https://images.unsplash.com/photo-1719937050601-969f4f25d060?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    
+ },
+ {
+    name:'sheriyans',
+    Followers: 10000,
+    Media:50,
+    Following:345,
+    imge1:'https://images.unsplash.com/photo-1734088900065-4e0570acdd7e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    img2:'https://images.unsplash.com/photo-1734127317339-ece0bb42738e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+ }
+ ]
+
+ var clutter = '';
+ arr.forEach(function (elem) {
+     clutter += `
+    <div class="card1"> 
+       <div class="c1">
+            <img src="${elem.imge1}" alt="">
+        </div>
+        <div class="c2">
+            <img src="${elem.img2}" alt="">
+        </div>
+    <div class="c4">
+        <h3>${elem.name}</h3>
+        <h4></h4>
+        <h5>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam voluptatum eligendi cupiditate facilis aliquam accusantium.</h5>
+    </div>
+
+    <div class="c5"></div>
+    <div class="c6">
+        <h2>${elem.Media}k<br>Media</h2>
+        <h2>${elem.Followers} <br>Followers</h2>
+        <h2>${elem.Following}<br>Following</h2>
+    </div>
+    </div>
+    `
+ });
+ 
+ 
+ var body = document.querySelector('.card');
+ body.innerHTML = clutter;
+ 
+          
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ // var body = document.querySelector('body')
+ // var cursor = document.querySelector('.c')
+ // var h1 = document.querySelector('h1')
+ 
+ // body.addEventListener('mousemove', function(hehe){
+ //       cursor.style.left = hehe.x + 'px';
+ //       cursor.style.top = hehe.y + 'px';
+ // })
+ 
+ // h1.addEventListener('mousemove',function(){
+ //    cursor.style.scale = 3
+ // })
+ 
+ // h1.addEventListener('mouseleave',function(){
+ //    cursor.style.scale = 1
+ // })
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ // variable insect
+ 
+ // var btn = document.querySelector('button')
+ // var body = document.querySelector('body')
+ 
+ // var images = ['https://www.freepnglogos.com/uploads/bug-png/bug-cgt-meurs-rebelles-morbihan-emploi-cent-27.png',
+ //    'https://static.vecteezy.com/system/resources/thumbnails/028/283/955/small_2x/cartoon-insect-aigenerative-png.png',
+ //    'https://e7.pngegg.com/pngimages/51/498/png-clipart-insect-cartoon-drawing-bug-animals-insects-thumbnail.png']
+ 
+ 
+ // btn.addEventListener('click',function(){
+ //    var b = images[Math.floor(Math.random()*3)]
+ //    var imge = document.createElement('img')
+ 
+ //    imge.setAttribute('src',b)
+ //    body.appendChild(imge)
+ 
+ //    var a = Math.floor(Math.random()*80)
+ //    var c = Math.floor(Math.random()*80)
+ //    var y = Math.floor(Math.random()*360)
+ 
+ //    imge.style.height= '90px'
+ //    imge.style.position = 'absolute'
+ //    imge.style.left = a + '%'
+ //    imge.style.top = c + '%'
+ //    imge.style.rotate = y + 'deg'
+    
+ 
+    
+ 
+ // })
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
